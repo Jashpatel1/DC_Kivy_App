29dcfbc827408542efdeb9ad2419c01d34bee911
mihirsakaria <sakaria.1@iitj.ac.in>
2020-11-16 01:39:05 +0530

    dynamic label added and gui improved

--- main.py
+++ main.py
@@ -3,42 +3,61 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.clock import Clock
+from kivy.uix.label import Label
+from kivy.uix.image import Image
 import socket
-#import sys
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+# import sys
+# class MyButton(Button):
+#     def on_release(self):
+#         new_button = Button(text = 'hello')
+#         self.ids.float_1.add_widget(new_button)
 
 class TestApp(App):
     def __init__(self):
         App.__init__(self)
         self.l = ""
         self.showData = ""
+        self.flag = False
+        self.layout = FloatLayout()
+        self.new_button = Button(background_color = (0,0,0,0),text = 'Connected to Server!',color = (0,0,0,1),pos_hint = {'center_x': 0.5, 'center_y' : 0.8},size_hint= (.20,.1))
+        self.img =  Image(source = 'bground.jpg',allow_stretch = True, keep_ratio = True)
 
     def build(self):
-        layout = FloatLayout()
+        #layout = FloatLayout()
+        #layout2 = FloatLayout()
+        #new_button = Button(text = 'hello',pos_hint = {'center_x': 0.5, 'center_y' : 0.9},size_hint= (.15,.1))
+        self.layout.add_widget(self.img)
+        connectbtn = Button(background_color = (255,255,0,0.4),text="Connect", pos_hint={'center_x': 0.2, 'center_y': 0.8},
+                            size_hint=(.15, .1), on_press=self.cconnect,on_release = self.ccnn)
+        self.layout.add_widget(connectbtn)
 
-        connectbtn = Button(text="Connect", pos_hint={'center_x': 0.2, 'center_y': 0.9},
-                                size_hint=(.15, .1), on_press=self.cconnect)
-        layout.add_widget(connectbtn)
+        disconnectbtn = Button(background_color = (255,255,0,0.4),text="Disconnect",pos_hint={'center_x': 0.8, 'center_y': 0.8},
+                               size_hint=(.15, .1), on_press=self.cdisconnect)
+        self.layout.add_widget(disconnectbtn)
 
-        disconnectbtn = Button(text="Disconnect", pos_hint={'center_x': 0.8, 'center_y': 0.9},
-                                size_hint=(.15, .1), on_press=self.cdisconnect)
-        layout.add_widget(disconnectbtn)
+        upbtn = Button(background_color = (0,0,0,0.7),text='Up', font_size=20, pos_hint={'center_x': 0.5, 'center_y': 0.45},
+                       size_hint=(.1, .1), on_press=self.up_press)
+        self.layout.add_widget(upbtn)
 
-        upbtn = Button(text='Up', font_size=20, pos_hint={'center_x': 0.5, 'center_y': 0.65},
-                        size_hint=(.1, .1), on_press=self.up_press)
-        layout.add_widget(upbtn)
+        dwnbtn = Button(background_color = (0,0,0,0.7),text='Down', font_size=20, pos_hint={'center_x': 0.5, 'center_y': 0.15},
+                        size_hint=(.1, .1), on_press=self.dwn_press)
+        self.layout.add_widget(dwnbtn)
 
-        dwnbtn = Button(text='Down', font_size=20, pos_hint={'center_x': 0.5, 'center_y': 0.35},
-                            size_hint=(.1, .1), on_press=self.dwn_press)
-        layout.add_widget(dwnbtn)
+        rbtn = Button(background_color = (0,0,0,0.7),text='Right', font_size=20, pos_hint={'center_x': 0.65, 'center_y': 0.3},
+                      size_hint=(.1, .1), on_press=self.r_press)
+        self.layout.add_widget(rbtn)
 
-        rbtn = Button(text='Right', font_size=20, pos_hint={'center_x': 0.65, 'center_y': 0.5},
-                    size_hint=(.1, .1), on_press=self.r_press)
-        layout.add_widget(rbtn)
+        lbtn = Button(background_color = (0,0,0,0.7),text='Left', font_size=20, pos_hint={'center_x': 0.35, 'center_y': 0.3},
+                      size_hint=(.1, .1), on_press=self.l_press)
+        self.layout.add_widget(lbtn)
+        return self.layout
 
-        lbtn = Button(text='Left', font_size=20, pos_hint={'center_x': 0.35, 'center_y': 0.5},
-                    size_hint=(.1, .1), on_press=self.l_press)
-        layout.add_widget(lbtn)
-        return layout
+    def ccnn(self, obj):
+        self.flag = True
 
     def cconnect(self, obj):
         try:
@@ -49,7 +68,10 @@ class TestApp(App):
             self.server_address = ('localhost', 10000)
             self.client.connect(self.server_address)
             print("Connecting to localhost, port 10000")
+            conn = Label(text="Connected")
+            # self.layout.add_widget(conn)
             self.showData = "Connected !"
+            self.layout.add_widget(self.new_button)
             Clock.schedule_interval(self.send_client, 0.3)
         except:
             self.showData = "Cannot Connect to Server.."
@@ -59,33 +81,34 @@ class TestApp(App):
         Clock.unschedule(self.send_client)
         self.client.close()
         self.showData = "Not Connected to server.."
+        self.layout.remove_widget(self.new_button)
         print("Disconnected from the Server..")
 
     def up_press(self, obj):
-        #l.append(+1)
+        # l.append(+1)
         self.l = 'U'
         print('Up button is pressed')
 
     def dwn_press(self, obj):
-        #l.append(-1)
+        # l.append(-1)
         self.l = 'D'
         print('Down button is pressed')
 
     def r_press(self, obj):
-        #l.append(+2)
+        # l.append(+2)
         self.l = 'R'
         print('Right button is pressed')
 
     def l_press(self, obj):
-        #l.append(-2)
+        # l.append(-2)
         self.l = 'L'
         print('Left button is pressed')
 
-    def send_client(self,obj):
+    def send_client(self, obj):
         val = ""
         if self.l == "":
             val = "Q"
-        else :
+        else:
             val = self.l
         self.l = ""
         try:
@@ -95,5 +118,6 @@ class TestApp(App):
         except:
             self.showData = "Error while sending data to server.."
             print("Error while sending data to server..")
+
 
 TestApp().run()
